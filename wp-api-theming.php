3182e1abfaa8c743c196acbbfd0d4ed88a2f84c9
Kirk Wight <kwight@kwight.ca>
2015-04-26 20:32:19 -0700

    Initial commit.

---
+++ wp-api-theming.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Plugin Name: WP-API Theming
+ * Description: Modifications to the WP-API plugin (`develop` branch) for theming.
+ * Author: kwight
+ * Author URI: http://kwight.ca
+ * Version: 0.1
+ * Plugin URI: https://github.com/kwight/wp-api-theming
+ * License: GPL2+
+ */
+
+/**
+ * Add properties to posts and pages endpoints.
+ */
+function wp_api_theming_posts_properties( $data ) {
+	// Set author's name.
+	$author = get_userdata( $data['author'] );
+	$data['author'] = array(
+		'id' => $author->ID,
+		'link' => get_author_posts_url( $author->ID ),
+		'name' => $author->data->display_name,
+	);
+
+	// Add post classes.
+	$data['post_class'] = get_post_class( $data['id'] );
+
+	// Add categories.
+	$categories = wp_api_theming_get_post_terms( $data['id'], 'category' );
+	if ( ! empty( $categories ) ) {
+		$data['categories'] = $categories;
+	}
+
+	// Add tags.
+	$tags = wp_api_theming_get_post_terms( $data['id'], 'post_tag' );
+	if ( ! empty( $tags ) ) {
+		$data['tags'] = $tags;
+	} 
+
+	return $data;
+}
+add_filter( 'json_prepare_post', 'wp_api_theming_posts_properties' );
+add_filter( 'json_prepare_page', 'wp_api_theming_posts_properties' );
+
+/**
+ * Get a post's terms with archive links.
+ */
+function wp_api_theming_get_post_terms( $id = false, $taxonomy = 'category' ) {
+	// We need an ID for this one.
+	if ( ! $id ) {
+		return false;
+	}
+
+	// Validate the taxonomy argument.
+	$valid_tax = apply_filters( 'wp_api_theming_valid_tax', array( 'category', 'post_tag' ) );
+	$taxonomy = ( in_array( $taxonomy, $valid_tax ) ) ? $taxonomy : 'category';
+
+	// Fetch our terms.
+	$terms = wp_get_post_terms( absint( $id ), $taxonomy );
+
+	if ( ! is_wp_error( $terms ) && ! empty( $terms ) ) {
+		// Append a link property to each term.
+		foreach ( $terms as $term ) {
+			$link = get_term_link( $term );
+			$term->link = $link;
+		}
+	}
+
+	return $terms;
+}
+
+
+
+
